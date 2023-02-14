427b1c5da5a2ba0a9e6b309be93708e71f035eed
H Oliver <helen.oliver@bcs.org.uk>
2023-02-14 17:48:55 +0000

    Format corrected

--- intro/cpu.c
+++ intro/cpu.c
@@ -4,30 +4,33 @@
 #include <sys/stat.h>
 #include <sys/time.h>
 
-double GetTime() {
+double GetTime() 
+{
     struct timeval t;
     int rc = gettimeofday(&t, NULL);
     assert(rc == 0);
     return (double) t.tv_sec + (double) t.tv_usec/1e6;
 }
 
-void Spin(int howlong) {
+void Spin(int howlong) 
+{
     double t = GetTime();
     while ((GetTime() - t) < (double) howlong)
-	; // do nothing in loop
+        ; // do nothing in loop
 }
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2) {
-	fprintf(stderr, "usage: cpu <string>\n");
-	exit(1);
+    if (argc != 2) 
+    {
+        fprintf(stderr, "usage: cpu <string>\n");
+        exit(1);
     }
     char *str = argv[1];
 
     while (1) {
-	printf("%s\n", str);
-	Spin(1);
+        printf("%s\n", str);
+        Spin(1);
     }
     return 0;
 }
