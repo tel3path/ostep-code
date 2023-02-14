ef4ab2d65d09812844653cdec3aeb35842ee2104
H Oliver <helen.oliver@bcs.org.uk>
2023-02-14 17:43:26 +0000

    Incorporated contents of common.h and removed the include

--- intro/cpu.c
+++ intro/cpu.c
@@ -1,6 +1,21 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "common.h"
+#include <sys/stat.h>
+#include <sys/time.h>
+
+double GetTime() {
+    struct timeval t;
+    int rc = gettimeofday(&t, NULL);
+    assert(rc == 0);
+    return (double) t.tv_sec + (double) t.tv_usec/1e6;
+}
+
+void Spin(int howlong) {
+    double t = GetTime();
+    while ((GetTime() - t) < (double) howlong)
+	; // do nothing in loop
+}
 
 int main(int argc, char *argv[])
 {
